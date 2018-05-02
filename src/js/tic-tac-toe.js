bf7a8c06a4088b25c33688d1250cb916f0f5229e
LukaGrdinic <1lukagrdinic@gmail.com>
2018-05-02 21:07:27 +0200

    Some game algorithm added
    The computer player can make a winning move, as long as the winning move is possible.
    Otherwise, it picks a random field.

--- src/js/tic-tac-toe.js
+++ src/js/tic-tac-toe.js
@@ -1,18 +1,21 @@
+// EVENT HANDLERS 
+// ===========================================================================================================
+
 // Setting the X and the O
-// =============================
+// =======================
 function setPlayer(playerSetup) {
-    player.playerSign = playerSetup;
+    game.playerSign = playerSetup;
     // Changing the color of the buttons
     let x = $('#X');
     let o = $('#O');
-    if (player.playerSign === "X") {
+    if (game.playerSign === "X") {
         setUserButtonColors(x);
         setComputerButtonColors(o);
-    } else if (player.playerSign === "O") {
+    } else if (game.playerSign === "O") {
         setUserButtonColors(o);
         setComputerButtonColors(x);
     }
-    player.assignComputerSign();
+    game.assignComputerSign();
 }
 // Setting the colors of player and computer
 // =========================================
@@ -27,9 +30,37 @@ function setComputerButtonColors(buttonOther) {
     css('color', '#2c2c2c').
     css('border', 'solid 1px transparent');
 }
-// The player Object
+
+// Handling the tic tac toe grid
 // =============================
-let player = {
+$('.tic-tac-toe-grid .col-4').on('click', function (e) {
+    var fieldNumber = parseInt(e.target.id);
+    $('#' + fieldNumber).text(game.playerSign);
+    // Doing the game Algorithm
+    gridArray[fieldNumber - 1] = game.playerSign;
+    updateFieldsLeft();
+    checkWinningMoves(); // This is the function I should check#
+    /* debugger; */
+    if (game.arrayOfWinningMoves.length !== 0) {
+        let winningField = game.arrayOfWinningMoves[0];
+        gridArray[winningField] = game.computerSign;
+        updateFieldsLeft();
+        let winningFieldId = winningField + 1;
+        $('#' + winningFieldId).text(game.computerSign);
+    } else {
+        setTimeout(function () {
+            pickRandomField();
+        }, 500);
+    }
+    checkForWinner(fieldNumber);
+});
+
+// ===========================================================================================================
+
+// THE MAIN GAME OBJECT
+// ====================
+
+let game = {
     playerSign: 'X',
     computerSign: 'O',
     assignComputerSign: function () {
@@ -38,24 +69,10 @@ let player = {
         } else {
             this.computerSign = 'X';
         }
-    }
+    },
+    lastMove: '',
+    arrayOfWinningMoves: []
 }
-
-// Handling the tic tac toe grid
-// =============================
-$('.tic-tac-toe-grid').on('click', function (e) {
-    debugger;
-    var fieldNumber = parseInt(e.target.id);
-    if (fieldNumber !== NaN) {
-        $('#' + fieldNumber).text(player.playerSign);
-        gridArray[fieldNumber - 1] = player.playerSign;
-    }
-    // Doing the game Algorithm
-    updateFieldsLeft();
-    setTimeout(function () {
-        pickRandomField();
-    }, 500)
-});
 
 // Game Algorithm
 
@@ -76,13 +93,37 @@ function pickRandomField() {
     var max = movesLeft - 1;
     var randomFieldIndex = Math.floor(Math.random() * (max - min + 1)) + min; // Random Number between min and max variables
     var randomFieldLeft = fieldsLeft[randomFieldIndex];
-    gridArray[randomFieldLeft - 1] = player.computerSign;
+    gridArray[randomFieldLeft - 1] = game.computerSign;
     updateFieldsLeft();
-    $('#' + randomFieldLeft).text(player.computerSign);
+    $('#' + randomFieldLeft).text(game.computerSign);
 }
 
+// Winning Algorithm
 
-// AFTER THE USER CLICKS, THE COMPUTER TAKES ITS TURN (AI STUFF)
+function winningSchemeBuilder(sign) { // this function should have a sign parameter
+    if (gridArray[0] === game[sign] && gridArray[1] === game[sign] && gridArray[2] === game[sign] || // What a big fucking if statement
+        gridArray[3] === game[sign] && gridArray[4] === game[sign] && gridArray[5] === game[sign] ||
+        gridArray[6] === game[sign] && gridArray[7] === game[sign] && gridArray[8] === game[sign] ||
+        gridArray[0] === game[sign] && gridArray[3] === game[sign] && gridArray[6] === game[sign] ||
+        gridArray[1] === game[sign] && gridArray[4] === game[sign] && gridArray[7] === game[sign] ||
+        gridArray[2] === game[sign] && gridArray[5] === game[sign] && gridArray[8] === game[sign] ||
+        gridArray[0] === game[sign] && gridArray[4] === game[sign] && gridArray[8] === game[sign] ||
+        gridArray[2] === game[sign] && gridArray[4] === game[sign] && gridArray[6] === game[sign]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkForWinner(lastMove, currentPlayer) {
+    var winningScheme = winningSchemeBuilder(currentPlayer);
+    if (winningScheme) {
+        window.alert('You lose!');
+        game.lastMove = lastMove;
+        console.log(game.lastMove);
+    }
+}
+
 
 // IF THE COMPUTER WINS THERE SHOULD BE A MESSAGE IN THE UPPER RIGHT CORNER THAT SAYS 'YOU LOOSE!'
 // IF THE COMPUTER WINS, THE SCREEN AROUND THE TIC TAC TOE GRID SHOULD SAY (WHISPER) LOOSER ... LOOSER ... LOOSER ... 
@@ -90,3 +131,61 @@ function pickRandomField() {
 // IF THE GAME IS A TIE , THERE SHOULD BE A MESSAGE IN THE UPPER RIGHT CORNER THAT SAYS 'A TIE!'
 
 // THE GAME SHOULD RESET 3 SECONDS AFTER
+
+/* MAIN ALGORITHM */
+/* ============== */
+
+// pickRandomField needs to become pickBestField
+
+// There needs to be a function that gives an aproximation of chances for user to win and computer to win
+
+function checkWinningMoves() { // function should check if there is a possible winning move
+    /* debugger; */
+    var numberOfWinningMoves = 0;
+    var arrayOfWinningMoves = [];
+    // Making a copy of the gridArray that should be used over and over again
+    var gridArrayBeforeCheck = [];
+    gridArray.forEach(function (oldFieldValue) {
+        gridArrayBeforeCheck.push(oldFieldValue);
+    });
+
+    for (var i = 0; i <= fieldsLeft.length; i++) {
+        var currentFieldObserved = fieldsLeft[i];
+
+        gridArray[currentFieldObserved - 1] = game.computerSign;
+        checkForWinner(currentFieldObserved - 1, 'computerSign');
+        if (game.lastMove !== '') {
+            console.log('The winning move is ' + game.lastMove);
+            numberOfWinningMoves++;
+            arrayOfWinningMoves.push(game.lastMove);
+            game.lastMove = '';
+        }
+        /* else {
+                   gridArray[currentFieldObserved - 1] = fieldsLeft[i];
+               } */
+        gridArray[currentFieldObserved - 1] = fieldsLeft[i]; // Maybe this line of code could replace gridArrayBeforeCheck stuff
+    }
+
+    gridArray = gridArrayBeforeCheck; // This part gets the old value of the gridArray,before the check
+    console.log('There are ' + numberOfWinningMoves + ' possible winning moves!');
+    console.log('Winning fields: ' + arrayOfWinningMoves);
+    // I should give an arrayOfWinningMoves to the player object
+    game.arrayOfWinningMoves = arrayOfWinningMoves;
+};
+
+/* EXAMPLE OF A CLOSURE */
+
+function closureThing() {
+    var myVar = false;
+    return function () {
+        if (myVar === false) {
+            myVar = true;
+            console.log(myVar);
+        } else {
+            myVar = false;
+            console.log(myVar);
+        }
+    }
+}
+
+var stuff = closureThing(); // Closure toggles true and false
