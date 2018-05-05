f7ca01c352f48ccf1e0d50c0254ffeaf825e748d
LukaGrdinic <1lukagrdinic@gmail.com>
2018-05-05 17:42:09 +0200

    The computer can prevent the player from winning and makes a winning move when possible

--- src/js/tic-tac-toe.js
+++ src/js/tic-tac-toe.js
@@ -15,7 +15,7 @@ function setPlayer(playerSetup) {
         setUserButtonColors(o);
         setComputerButtonColors(x);
     }
-    game.assignComputerSign();
+    assignComputerSign();
 }
 // Setting the colors of player and computer
 // =========================================
@@ -34,25 +34,28 @@ function setComputerButtonColors(buttonOther) {
 // Handling the tic tac toe grid
 // =============================
 $('.tic-tac-toe-grid .col-4').on('click', function (e) {
-    var fieldNumber = parseInt(e.target.id);
-    $('#' + fieldNumber).text(game.playerSign);
-    // Doing the game Algorithm
-    gridArray[fieldNumber - 1] = game.playerSign;
-    updateFieldsLeft();
-    checkWinningMoves(); // This is the function I should check#
-    /* debugger; */
-    if (game.arrayOfWinningMoves.length !== 0) {
-        let winningField = game.arrayOfWinningMoves[0];
-        gridArray[winningField] = game.computerSign;
-        updateFieldsLeft();
-        let winningFieldId = winningField + 1;
-        $('#' + winningFieldId).text(game.computerSign);
-    } else {
-        setTimeout(function () {
-            pickRandomField();
-        }, 500);
+    debugger;
+    if (game.playerTurn !== 'computer') {
+        if (game.winner === '') {
+            var fieldNumber = parseInt(e.target.id);
+            $('#' + fieldNumber).text(game.playerSign);
+            game.gridArray[fieldNumber - 1] = game.playerSign;
+            game.playerTurn = 'computer';
+            updateFieldsAndMovesLeft(); // Updates the situation of the game
+            makeWinningMove(); // Checks if there are possible winning moves
+            preventPlayerFromWinning(); // Checks if it should prevent the player from making a winning move
+            setTimeout(function () {
+                pickRandomField();
+            }, 500);
+            checkForWinner(fieldNumber, 'playerSign');
+            checkForWinner(game.lastMove, 'computerSign');
+            /* declareWinner(); */
+
+        } else {
+            window.alert('The game is over! No more playing!');
+        }
     }
-    checkForWinner(fieldNumber);
+
 });
 
 // ===========================================================================================================
@@ -63,52 +66,54 @@ $('.tic-tac-toe-grid .col-4').on('click', function (e) {
 let game = {
     playerSign: 'X',
     computerSign: 'O',
-    assignComputerSign: function () {
-        if (this.playerSign === 'X') {
-            this.computerSign = 'O';
-        } else {
-            this.computerSign = 'X';
-        }
-    },
+    playerTurn: '',
+    gridArray: ['1', '2', '3', '4', '5', '6', '7', '8', '9'], // Tic Tac Toe fields placeholder,
     lastMove: '',
-    arrayOfWinningMoves: []
+    arrayOfWinningMoves: [],
+    fieldsLeft: [], // Fields that have still not been used in the game
+    movesLeft: 0, // Number of moves left in the game
+    winner: ''
 }
 
-// Game Algorithm
+function assignComputerSign() {
+    if (game.playerSign === 'X') {
+        game.computerSign = 'O';
+    } else {
+        game.computerSign = 'X';
+    }
+}
 
-var gridArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9']; // Tic Tac Toe fields placeholder
-
-var fieldsLeft; // Fields that have still not been used
-var movesLeft; // Moves left in the game
-
-function updateFieldsLeft() {
-    fieldsLeft = gridArray.filter(function (field) {
+function updateFieldsAndMovesLeft() {
+    game.fieldsLeft = game.gridArray.filter(function (field) {
         return field !== 'X' && field !== 'O';
     });
-    movesLeft = fieldsLeft.length;
+    game.movesLeft = game.fieldsLeft.length;
 }
 
 function pickRandomField() {
-    var min = 0;
-    var max = movesLeft - 1;
-    var randomFieldIndex = Math.floor(Math.random() * (max - min + 1)) + min; // Random Number between min and max variables
-    var randomFieldLeft = fieldsLeft[randomFieldIndex];
-    gridArray[randomFieldLeft - 1] = game.computerSign;
-    updateFieldsLeft();
-    $('#' + randomFieldLeft).text(game.computerSign);
+    if (game.playerTurn === 'computer') {
+        var min = 0;
+        var max = game.movesLeft - 1;
+        var randomFieldIndex = Math.floor(Math.random() * (max - min + 1)) + min; // Random Number between min and max variables
+        var randomFieldLeft = game.fieldsLeft[randomFieldIndex];
+        game.gridArray[randomFieldLeft - 1] = game.computerSign;
+        updateFieldsAndMovesLeft();
+        $('#' + randomFieldLeft).text(game.computerSign);
+        game.playerTurn = 'player';
+    }
 }
 
 // Winning Algorithm
 
 function winningSchemeBuilder(sign) { // this function should have a sign parameter
-    if (gridArray[0] === game[sign] && gridArray[1] === game[sign] && gridArray[2] === game[sign] || // What a big fucking if statement
-        gridArray[3] === game[sign] && gridArray[4] === game[sign] && gridArray[5] === game[sign] ||
-        gridArray[6] === game[sign] && gridArray[7] === game[sign] && gridArray[8] === game[sign] ||
-        gridArray[0] === game[sign] && gridArray[3] === game[sign] && gridArray[6] === game[sign] ||
-        gridArray[1] === game[sign] && gridArray[4] === game[sign] && gridArray[7] === game[sign] ||
-        gridArray[2] === game[sign] && gridArray[5] === game[sign] && gridArray[8] === game[sign] ||
-        gridArray[0] === game[sign] && gridArray[4] === game[sign] && gridArray[8] === game[sign] ||
-        gridArray[2] === game[sign] && gridArray[4] === game[sign] && gridArray[6] === game[sign]) {
+    if (game.gridArray[0] === game[sign] && game.gridArray[1] === game[sign] && game.gridArray[2] === game[sign] || // What a big fucking if statement
+        game.gridArray[3] === game[sign] && game.gridArray[4] === game[sign] && game.gridArray[5] === game[sign] ||
+        game.gridArray[6] === game[sign] && game.gridArray[7] === game[sign] && game.gridArray[8] === game[sign] ||
+        game.gridArray[0] === game[sign] && game.gridArray[3] === game[sign] && game.gridArray[6] === game[sign] ||
+        game.gridArray[1] === game[sign] && game.gridArray[4] === game[sign] && game.gridArray[7] === game[sign] ||
+        game.gridArray[2] === game[sign] && game.gridArray[5] === game[sign] && game.gridArray[8] === game[sign] ||
+        game.gridArray[0] === game[sign] && game.gridArray[4] === game[sign] && game.gridArray[8] === game[sign] ||
+        game.gridArray[2] === game[sign] && game.gridArray[4] === game[sign] && game.gridArray[6] === game[sign]) {
         return true;
     } else {
         return false;
@@ -118,19 +123,16 @@ function winningSchemeBuilder(sign) { // this function should have a sign parame
 function checkForWinner(lastMove, currentPlayer) {
     var winningScheme = winningSchemeBuilder(currentPlayer);
     if (winningScheme) {
-        window.alert('You lose!');
+        declareWinner();
         game.lastMove = lastMove;
-        console.log(game.lastMove);
     }
 }
 
-
-// IF THE COMPUTER WINS THERE SHOULD BE A MESSAGE IN THE UPPER RIGHT CORNER THAT SAYS 'YOU LOOSE!'
-// IF THE COMPUTER WINS, THE SCREEN AROUND THE TIC TAC TOE GRID SHOULD SAY (WHISPER) LOOSER ... LOOSER ... LOOSER ... 
-
-// IF THE GAME IS A TIE , THERE SHOULD BE A MESSAGE IN THE UPPER RIGHT CORNER THAT SAYS 'A TIE!'
-
-// THE GAME SHOULD RESET 3 SECONDS AFTER
+function declareWinner() {
+    if (game.winner !== '') {
+        window.alert(game.winner + ' wins!');
+    }
+}
 
 /* MAIN ALGORITHM */
 /* ============== */
@@ -139,39 +141,77 @@ function checkForWinner(lastMove, currentPlayer) {
 
 // There needs to be a function that gives an aproximation of chances for user to win and computer to win
 
-function checkWinningMoves() { // function should check if there is a possible winning move
+/* THE AI ALGORITHM */
+
+function makeWinningMove() { // function should check if there is a possible winning move
+
     /* debugger; */
-    var numberOfWinningMoves = 0;
-    var arrayOfWinningMoves = [];
-    // Making a copy of the gridArray that should be used over and over again
-    var gridArrayBeforeCheck = [];
-    gridArray.forEach(function (oldFieldValue) {
-        gridArrayBeforeCheck.push(oldFieldValue);
-    });
 
-    for (var i = 0; i <= fieldsLeft.length; i++) {
-        var currentFieldObserved = fieldsLeft[i];
+    if (game.playerTurn === 'computer') {
 
-        gridArray[currentFieldObserved - 1] = game.computerSign;
-        checkForWinner(currentFieldObserved - 1, 'computerSign');
-        if (game.lastMove !== '') {
-            console.log('The winning move is ' + game.lastMove);
-            numberOfWinningMoves++;
-            arrayOfWinningMoves.push(game.lastMove);
-            game.lastMove = '';
+        /* These are some variables to be used if there is no break; in the for loop */
+        var numberOfWinningMoves = 0;
+        var arrayOfWinningMoves = [];
+
+        for (var i = 0; i <= game.fieldsLeft.length; i++) {
+            var currentFieldObserved = game.fieldsLeft[i];
+
+            game.gridArray[currentFieldObserved - 1] = game.computerSign;
+            checkForWinner(currentFieldObserved - 1, 'computerSign');
+            if (game.lastMove !== '') {
+                numberOfWinningMoves++;
+                if (numberOfWinningMoves === 1) {
+                    game.gridArray[currentFieldObserved - 1] = game.computerSign;
+                    let winningField = game.lastMove + 1;
+                    $('#' + winningField).text(game.computerSign);
+                    game.playerTurn = 'player';
+                    game.winner = 'Computer';
+                    break;
+                }
+                arrayOfWinningMoves.push(game.lastMove);
+                game.lastMove = '';
+            }
+            game.gridArray[currentFieldObserved - 1] = game.fieldsLeft[i];
         }
-        /* else {
-                   gridArray[currentFieldObserved - 1] = fieldsLeft[i];
-               } */
-        gridArray[currentFieldObserved - 1] = fieldsLeft[i]; // Maybe this line of code could replace gridArrayBeforeCheck stuff
+        game.arrayOfWinningMoves = arrayOfWinningMoves;
     }
-
-    gridArray = gridArrayBeforeCheck; // This part gets the old value of the gridArray,before the check
-    console.log('There are ' + numberOfWinningMoves + ' possible winning moves!');
-    console.log('Winning fields: ' + arrayOfWinningMoves);
-    // I should give an arrayOfWinningMoves to the player object
-    game.arrayOfWinningMoves = arrayOfWinningMoves;
 };
+
+function preventPlayerFromWinning() { // function should stop the player from making a winning move
+
+    if (game.playerTurn === 'computer') {
+
+        for (var i = 0; i <= game.fieldsLeft.length; i++) {
+            var currentFieldObserved = game.fieldsLeft[i];
+
+            game.gridArray[currentFieldObserved - 1] = game.playerSign;
+            checkForWinner(currentFieldObserved - 1, 'playerSign');
+
+            if (game.lastMove !== '') {
+                game.gridArray[currentFieldObserved - 1] = game.computerSign;
+                let winningField = game.lastMove + 1;
+                $('#' + winningField).text(game.computerSign);
+                game.lastMove = '';
+                game.playerTurn = 'player';
+                break;
+            }
+            game.gridArray[currentFieldObserved - 1] = game.fieldsLeft[i];
+        }
+    }
+}
+
+// There should be a function that calculates the chance of the situation on the grid
+// - If there is gridArray[0] and gridArray[8] , there should be a better chance (more points) because it matches winning more winningScheme builder lines 
+
+/* THINGS TO BE DONE */
+//====================
+
+// IF THE COMPUTER WINS THERE SHOULD BE A MESSAGE IN THE UPPER RIGHT CORNER THAT SAYS 'YOU LOOSE!'
+// IF THE COMPUTER WINS, THE SCREEN AROUND THE TIC TAC TOE GRID SHOULD SAY (WHISPER) LOOSER ... LOOSER ... LOOSER ... 
+
+// IF THE GAME IS A TIE , THERE SHOULD BE A MESSAGE IN THE UPPER RIGHT CORNER THAT SAYS 'A TIE!'
+
+// THE GAME SHOULD RESET 3 SECONDS AFTER
 
 /* EXAMPLE OF A CLOSURE */
 
